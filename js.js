c6098eca457acca51ee66877a62d44c15fd16c23
Pablo Monteserín <pablomonteserin@pablomonteserin.com>
2024-05-29 21:40:11 +0200

    subida

--- js.js
+++
@@ -1,5 +0,0 @@
-const obj = { a: 1, b: 2, c: 3 };
-const { c, b, a } = obj;
-console.log(a);
-console.log(b);
-console.log(c);
--- rqr.js
+++
@@ -1,9 +0,0 @@
-export const getFilmnsFromURls = () =>{
-  const data = await Promise.all(
-  ids.map(async (id) => {
-    const r = await axios.get("https://example.com/" + id);
-    return r.data;
-  })
-);
-return data;
-}
